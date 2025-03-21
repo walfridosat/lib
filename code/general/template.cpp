06eb4af69cf133d0063af9bf056f5fd6d57bf22d
Walfrido Saturno <walfrido.saturno@owlshard.com>
2025-03-21 11:32:24 -0300

    asd

---
+++ code/general/template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define FORI(n) for(ll i = 0; i < (n); i++)
+#define FORJ(n) for(ll j = 0; j < (n); j++)
+#define endl '\n'
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define pii pair<ll, ll>
+
+void solve(){
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+    int t = 1;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
